9ba04e2730a060e0276e30f5612244085d9c0d97
strackerfoxx <86536635+strackerfoxx@users.noreply.github.com>
2023-06-27 00:13:50 -0600

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+import express from "express"
+const router = express.Router()
+import { create, login, verify, forget, newPass, profileImage, getUser, getAllUsers} from "../controller/UserController.js"
+import { auth } from "../middleware/auth.js"
+import { newProfileImage } from "../controller/FileController.js"
+
+router.post("/", create)
+router.post("/login", login)
+router.get("/verify", verify)
+router.post("/forget", forget)
+router.post("/newpassword", newPass)
+router.post("/image", auth, newProfileImage, profileImage)
+router.get("/posts", getUser)
+router.get("/all", getAllUsers)
+
+export default router 
